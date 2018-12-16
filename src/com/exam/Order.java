df8ddf63dc39185671e0f721c3232b795a0dd5d0
Andy811 <andygood811@gmail.com>
2018-12-17 03:51:37 +0800

    homework

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -2,16 +2,24 @@ package com.exam;
 
 import java.util.ArrayList;
 
+import org.junit.Test;
+
 public class Order {
 	ArrayList t = new ArrayList();
-	
-	public void add(Ticket ticket) {
-		t.add(ticket);
 
+	@Test
+	public void add(Ticket ticket) {
+		t.add(ticket.origin);
+		t.add(ticket.destination);
+		t.add(ticket.price);
+		t.add(ticket.quantity);
+		t.add(ticket.price*ticket.quantity);
+		
 	}
 
+	@Test
 	public void print() {
-		System.out.print(new Ticket().result);
+		System.out.print(t);
 	}
 
 }
--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -3,15 +3,23 @@ package com.exam;
 import java.util.ArrayList;
 
 public class Ticket {
-Order order = new Order();
-String result;	
-public Ticket(){
-	
-}
-public Ticket(String origin,String destination,int price,int quantity){
-	
-		 result = origin+" "+destination+" "+price+" "+quantity;
-		
+	Order order = new Order();
+	String origin;
+	String destination;
+	int price;
+	int quantity;
+
+	public Ticket() {
+
+	}
+
+	public Ticket(String origin, String destination, int price, int quantity) {
+
+		this.origin = origin;
+		this.destination = destination;
+		this.price = price;
+		this.quantity = quantity;
+
 	}
 
 }
